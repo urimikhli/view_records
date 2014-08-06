3abc801f309de3243d99d6805476e6fc91b6091d
Uri Mikhli <urimikhli@gmail.com>
2014-08-06 17:05:13 -0400

    lets get started with tdd

--- spec/manage_records_spec.rb
+++ spec/manage_records_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper'
 
+describe "manage records" do
+
+  context "Initializing the records by reading the files" do
+    before do
+      records = DataRecords.new()
+    end
+    it "should instantiate a class instance with the records we want" do
+      records.should respond_to to_s
+    end
+  end
+end
+
