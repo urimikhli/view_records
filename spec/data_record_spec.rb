b3f9c1096d446f11f6c4c56a23d3293a57a252f1
Uri Mikhli <urimikhli@gmail.com>
2014-08-12 09:44:20 -0400

    switch data_record spec to using 'let' rather then instance var

--- spec/data_record_spec.rb
+++ spec/data_record_spec.rb
@@ -3,13 +3,14 @@ require './data_record'
 
 describe DataRecord do
   
+  let(:record) { DataRecord.new("Bonk | Radek | S | M | Green | 6-3-1975") }
   context ".record_output" do
     before do
-      @record = DataRecord.new("Bonk | Radek | S | M | Green | 6-3-1975")
+     # @record = DataRecord.new("Bonk | Radek | S | M | Green | 6-3-1975")
     end
 
     it "should instantiate a class instance with the records we want" do
-      expect(@record.record_output).to eq "Bonk Radek Male 6/3/1975 Green"
+      expect(record.record_output).to eq "Bonk Radek Male 6/3/1975 Green"
     end
   end
 end
