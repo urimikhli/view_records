afde584c60f0fcc0f447678cf8ac6199bb7f97e2
Uri Mikhli <urimikhli@gmail.com>
2014-08-12 09:47:56 -0400

    switch data_record_list spec to 'let' rather then instance vars

--- spec/data_record_list_spec.rb
+++ spec/data_record_list_spec.rb
@@ -4,56 +4,54 @@ require './data_record_list'
 
 describe DataRecordList do
   
-  before do
-    @recordList = DataRecordList.new
-    @list = []
-  end
+  let(:recordList) { DataRecordList.new }
+  let(:list) { Array.new }
 
   it "should return blank list" do
-    expect(@recordList.record_list).to eq []
+    expect(recordList.record_list).to eq []
   end
   
   context ".append" do
     before do
-      @recordList.append(DataRecord.new("Bonk | Radek | S | M | Green | 6-3-1975")) 
+      recordList.append(DataRecord.new("Bonk | Radek | S | M | Green | 6-3-1975")) 
     end
 
     it "should add the record to DataRecordList" do
-      expect(@recordList.record_list.size).to eq 1
+      expect(recordList.record_list.size).to eq 1
     end
     
   end
   context ".output_records" do
     before do
-      @recordList.append(DataRecord.new("Bonk | Radek | S | M | Green | 6-3-1975"))
-      @recordList.append(DataRecord.new("Kournikova Anna F F 6-3-1975 Red"))
-      @recordList.append(DataRecord.new("Abercrombie, Neil, Male, Tan, 2/13/1943"))
+      recordList.append(DataRecord.new("Bonk | Radek | S | M | Green | 6-3-1975"))
+      recordList.append(DataRecord.new("Kournikova Anna F F 6-3-1975 Red"))
+      recordList.append(DataRecord.new("Abercrombie, Neil, Male, Tan, 2/13/1943"))
     end
 
     context "sort by gender" do
       before do
-        @recordList.output_records("gender")
+        recordList.output_records("gender")
       end
       it "should show the Female record first" do
-        expect(@recordList.record_list.first.firstName).to eq "Anna"
+        expect(recordList.record_list.first.firstName).to eq "Anna"
       end 
     end
 
     context "sort by birthdate" do
       before do
-        @recordList.output_records("birthdate")
+        recordList.output_records("birthdate")
       end
       it "should show the oldest birthday first" do
-        expect(@recordList.record_list.first.dateOfBirth).to eq "2/13/1943"
+        expect(recordList.record_list.first.dateOfBirth).to eq "2/13/1943"
       end 
     end
 
     context "sort by lastname" do
       before do
-        @recordList.output_records("birthdate")
+        recordList.output_records("birthdate")
       end
       it "should show Abercrombie last name first" do
-        expect(@recordList.record_list.first.lastName).to eq "Abercrombie"
+        expect(recordList.record_list.first.lastName).to eq "Abercrombie"
       end 
     end
 
